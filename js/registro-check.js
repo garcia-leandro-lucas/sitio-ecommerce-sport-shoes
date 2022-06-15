7b9c62e0307993271e5272ed1ff86f1ab6f93838
garcia-leandro <garcia.leandrolucas@gmail.com>
2022-06-15 18:08:02 -0300

    Se agrega para correccion

--- js/registro-check.js
+++ js/registro-check.js
@@ -1,1 +1,18 @@
+let arrayUsuarios = Usuario;
+const recuperarStorage = JSON.parse(localStorage.getItem("listasDeUsuarios"));
+
+if(recuperarStorage) {
+    arrayUsuarios = recuperarStorage;
+}
+
+console.log(arrayUsuarios.nombre); 
+console.log(arrayUsuarios.apellido); 
+console.log(arrayUsuarios.dni); 
+console.log(arrayUsuarios.localidad);  // Estos datos son los que necesito imprimir. Pero no pasa nada. No se como pensar en el manejo de los datos
+
+
+// console.log(recuperarStorage.nombre); 
+// console.log(recuperarStorage.apellido); 
+// console.log(recuperarStorage.dni); 
+// console.log(recuperarStorage.localidad);
 
--- js/registro-de-usuario.js
+++ js/registro-de-usuario.js
@@ -24,70 +24,71 @@ enviar_validacio_de_registro.addEventListener('click', () => {// aca conviene co
         let nuevo_usuario = new Usuario(nombre.value, apellido.value, dni.value, localidad.value, fecha_de_nacimiento.value, genero.value, email.value, contrasenia.value, confirmacion_de_contrasenia.value);
 
         guardar_usuario(nuevo_usuario);
+        console.log(nuevo_usuario);
 
         let validacion_de_datos_usuario = mensaje_de_confirmacion(nuevo_usuario);
         let check = true;
         //console.log("Estoy antes de while check");
     
-        while(check){
-            console.log("Estoy dentro de while check");
-            if(validacion_de_datos_usuario) {
-                console.log("Estoy dentro de while y del check");
-                console.log(nuevo_usuario);
-                nuevo_usuario.mostrar_bienvenida();
-                check = false;
+        // while(check){
+        //     console.log("Estoy dentro de while check");
+        //     if(validacion_de_datos_usuario) {
+        //         console.log("Estoy dentro de while y del check");
+        //         console.log(nuevo_usuario);
+        //         nuevo_usuario.mostrar_bienvenida();
+        //         check = false;
                         
-            }else {
+        //     }else {
                         
-                let datos = parseInt(prompt("¿Qué datos quisieras modificar?\nPor favor, seleccione un número según corresponda la opción a modificar:\n1) Nombre\n2) Apellido\n3) D.N.I.\n4) Localidad\n5) Fecha de nacimiento\n6) Género\n7) Email\n8) Ninguno. Los datos estan correctos."));
+        //         let datos = parseInt(prompt("¿Qué datos quisieras modificar?\nPor favor, seleccione un número según corresponda la opción a modificar:\n1) Nombre\n2) Apellido\n3) D.N.I.\n4) Localidad\n5) Fecha de nacimiento\n6) Género\n7) Email\n8) Ninguno. Los datos estan correctos."));
         
-                switch(datos){
-                    case 1:
-                        let nombre = prompt("Ingrese su nombre:");
-                        nuevo_usuario.nombre = nombre;
-                        break;
+        //         switch(datos){
+        //             case 1:
+        //                 let nombre = prompt("Ingrese su nombre:");
+        //                 nuevo_usuario.nombre = nombre;
+        //                 break;
                             
-                        case 2:
-                            let apellido = prompt("Ingrese su apellido:");
-                            nuevo_usuario.apellido = apellido;
-                            break;
+        //                 case 2:
+        //                     let apellido = prompt("Ingrese su apellido:");
+        //                     nuevo_usuario.apellido = apellido;
+        //                     break;
                             
-                        case 3:
-                            let dni = prompt("Ingrese su D.N.I.::");
-                            nuevo_usuario.dni = dni;
-                            break;
+        //                 case 3:
+        //                     let dni = prompt("Ingrese su D.N.I.::");
+        //                     nuevo_usuario.dni = dni;
+        //                     break;
         
-                        case 4:
-                            let localidad = prompt("Ingrese su localidad:");
-                            nuevo_usuario.localidad = localidad;
-                            break;
+        //                 case 4:
+        //                     let localidad = prompt("Ingrese su localidad:");
+        //                     nuevo_usuario.localidad = localidad;
+        //                     break;
         
-                        case 5:
-                            let fecha_de_nacimiento = prompt("Ingrese su fecha de nacimiento:");
-                            nuevo_usuario.fecha_de_nacimiento = fecha_de_nacimiento;
-                            break;
+        //                 case 5:
+        //                     let fecha_de_nacimiento = prompt("Ingrese su fecha de nacimiento:");
+        //                     nuevo_usuario.fecha_de_nacimiento = fecha_de_nacimiento;
+        //                     break;
         
-                        case 6:
-                            let genero = prompt("Ingrese su género:");
-                            nuevo_usuario.genero = genero;
-                            break;
+        //                 case 6:
+        //                     let genero = prompt("Ingrese su género:");
+        //                     nuevo_usuario.genero = genero;
+        //                     break;
         
-                        case 7:
-                            let email = prompt("Ingrese su género:");
-                            nuevo_usuario.email = email;
-                            break;
+        //                 case 7:
+        //                     let email = prompt("Ingrese su género:");
+        //                     nuevo_usuario.email = email;
+        //                     break;
                             
-                        case 8:
-                            validacion_de_datos_usuario = true;
-                            nuevo_usuario.mostrar_bienvenida();
-                            console.log(nuevo_usuario);
-                            break
-                        }
+        //                 case 8:
+        //                     validacion_de_datos_usuario = true;
+        //                     nuevo_usuario.mostrar_bienvenida();
+        //                     console.log(nuevo_usuario);
+        //                     break
+        //                 }
     
-                        validacion_de_datos_usuario = mensaje_de_confirmacion(nuevo_usuario);
-                    }
+        //                 validacion_de_datos_usuario = mensaje_de_confirmacion(nuevo_usuario);
+        //             }
     
-        }
+        // }
         }else {
             swal("Por favor, debés completar todos los datos que estan solicitados en el formulario");
         }
@@ -103,19 +104,23 @@ enviar_validacio_de_registro.addEventListener('click', () => {// aca conviene co
 */
 function mensaje_de_confirmacion(usuario){
 
-    let mensaje = "";
-    
-    mensaje = `${mensaje} Por favor, corroborar que los datos ingresados son correctos:`;
-    mensaje = `${mensaje} \n Nombre: ${usuario.nombre}`;
-    mensaje = `${mensaje} \n Apellido: ${usuario.apellido}`;
-    mensaje = `${mensaje} \n D.N.I.: ${usuario.dni}`;
-    mensaje = `${mensaje} \n Localidad: ${usuario.localidad}`;
-    mensaje = `${mensaje} \n Fecha de Nacimiento: ${usuario.fecha_de_nacimiento}`;
-    mensaje = `${mensaje} \n Género: ${usuario.genero}`;
-    mensaje = `${mensaje} \n E-mail: ${usuario.email}`;
+    let info = document.getElementById('datos');
+    info.innerHTML = `
+    `
 
-    let confirmacion = confirm(mensaje);
-    return confirmacion
+    // let mensaje = "";
+    
+    // mensaje = `${mensaje} Por favor, corroborar que los datos ingresados son correctos:`;
+    // mensaje = `${mensaje} \n Nombre: ${usuario.nombre}`;
+    // mensaje = `${mensaje} \n Apellido: ${usuario.apellido}`;
+    // mensaje = `${mensaje} \n D.N.I.: ${usuario.dni}`;
+    // mensaje = `${mensaje} \n Localidad: ${usuario.localidad}`;
+    // mensaje = `${mensaje} \n Fecha de Nacimiento: ${usuario.fecha_de_nacimiento}`;
+    // mensaje = `${mensaje} \n Género: ${usuario.genero}`;
+    // mensaje = `${mensaje} \n E-mail: ${usuario.email}`;
+
+    // let confirmacion = confirm(mensaje);
+    // return confirmacion
     
 }
 

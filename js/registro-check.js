966721ca4ed4b482bbf7c5e7d1cb1d5547df18e3
garcia-leandro <garcia.leandrolucas@gmail.com>
2022-06-20 22:19:05 -0300

    Se agrega los datos de usuario

--- js/registro-check.js
+++ js/registro-check.js
@@ -10,12 +10,26 @@ if(recuperarStorage) {
 
     let  {
 
-        nombre, apellido, dni, localidad
+        nombre, apellido, dni, localidad, fecha_de_nacimiento, genero, email
 
     } = arrayUsuarios[0];
 
-    let nombreid = document.getElementById('nombreid');
-    nombreid.innerText = nombre;
+    let usuario_nombre = document.getElementById('nombre');
+    let usuario_apellido = document.getElementById('apellido');
+    let usuario_dni = document.getElementById('dni');
+    let usuario_localidad = document.getElementById('localidad');
+    let usuario_fecha_de_nacimiento = document.getElementById('fecha-de-nacimiento');
+    let usuario_genero = document.getElementById('genero');
+    let usuario_email = document.getElementById('email');
+    
+    usuario_nombre.innerText = nombre;
+    usuario_apellido.innerText = apellido;
+    usuario_dni.innerText = dni;
+    usuario_localidad.innerText = localidad;
+    usuario_fecha_de_nacimiento.innerText = fecha_de_nacimiento;
+    usuario_genero.innerText = genero;
+    usuario_email.innerText = email;
+
 
     // let nombreid = document.getElementById('nombreid');
     // nombreid.innerText = usuario.nombre;
